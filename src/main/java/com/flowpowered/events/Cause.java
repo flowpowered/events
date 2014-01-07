aa0ae72ed8ac834c9b05fdf4ccccd750f1eab6de
Wolf480pl <wolf480@interia.pl>
2014-01-07 19:58:23 +0100

    Causes should carry the first cause with them, cause iterating through all the parents is silly.

--- src/main/java/com/flowpowered/events/Cause.java
+++ src/main/java/com/flowpowered/events/Cause.java
@@ -29,57 +29,47 @@ package com.flowpowered.events;
  * @param <T> source of the cause
  */
 public abstract class Cause<T> {
-	private static final int MAX_CAUSES = 100;
-	private final Cause<?> parent;
+    private final Cause<?> parent;
+    private final Cause<?> firstCause;
 
-	/**
-	 * Gets the source of the action
-	 */
-	public abstract T getSource();
+    /**
+     * Gets the source of the action
+     */
+    public abstract T getSource();
 
-	/**
-	 * Constructs a cause with no parent cause
-	 */
-	public Cause() {
-		this(null);
-	}
+    /**
+     * Constructs a cause with no parent cause
+     */
+    public Cause() {
+        this(null);
+    }
 
-	/**
-	 * Constructs a cause with a parent cause that was directly responsible for the action
-	 */
-	public Cause(Cause<?> parent) {
-		this.parent = parent;
-	}
+    /**
+     * Constructs a cause with a parent cause that was directly responsible for the action
+     */
+    public Cause(Cause<?> parent) {
+        this.parent = parent;
+        this.firstCause = parent != null ? parent.firstCause : null;
+    }
 
-	/**
-	 * Gets the first cause in the parent-child series of causes that led to this.
-	 *
-	 * <p>May terminate early to prevent infinite loops</p>
-	 *
-	 * Note: Can be null if there is no parent
-	 *
-	 * @return first cause or null if there is no parent
-	 */
-	public final Cause<?> getFirstCause() {
-		int causes = 0;
-		Cause<?> main = this;
-		while (causes < MAX_CAUSES) {
-			if (main.getParent() != null) {
-				main = main.getParent();
-			} else {
-				break;
-			}
-		}
-		return main;
-	}
+    /**
+     * Gets the first cause in the parent-child series of causes that led to this.
+     *
+     * Note: Can be null if there is no parent
+     *
+     * @return first cause or null if there is no parent
+     */
+    public final Cause<?> getFirstCause() {
+        return firstCause;
+    }
 
-	/**
-	 * Gets the parent cause of this cause. <br/><br/> Note: Can be null if there is no parent
-	 *
-	 * @return parent cause or null if there is no parent
-	 */
-	public final Cause<?> getParent() {
-		return parent;
-	}
+    /**
+     * Gets the parent cause of this cause. <br/><br/> Note: Can be null if there is no parent
+     *
+     * @return parent cause or null if there is no parent
+     */
+    public final Cause<?> getParent() {
+        return parent;
+    }
 }
 
